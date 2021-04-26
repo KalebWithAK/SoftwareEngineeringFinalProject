80315e1d43cdbe636e818fa0deb29146e926e82c
KalebWithAK <kad9603@gmail.com>
2021-04-26 13:15:27 -0400

    html and css set from state not props

--- web/src/components/post.js
+++ web/src/components/post.js
@@ -2,8 +2,21 @@ import React from 'react'
 import Parser from 'html-react-parser'
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            html: '',
+            css: '',
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+
     render() {
-        const { html, css } = this.props
+        const { html, css } = this.state
 
         // parser renders the html and css for the selected page
         // not sure if we'll still include the title and author on this page or just render the html
