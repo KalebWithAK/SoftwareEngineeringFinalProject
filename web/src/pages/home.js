abe22f759251cd668659533362a67c6cf7621488
KalebWithAK <kad9603@gmail.com>
2021-04-26 13:15:59 -0400

    previews on homepage not previewLayout

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import PreviewLayout from '../components/previewLayout.js'
+import Preview from '../components/preview.js'
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [
+                { creator: 'Kaleb', title: 'hi there', html: '<h1>Example Preview</h1><p>This is an example of an html file to be rendered in a post preview component.I want to use an iframe inside of the post preview component but since an html src is required, I\'m creating this file. That should be enough text. But just in case, boogilaboo.</p>', css: 'h1 { background: red }' }
+            ]
+        }
+    }
+
+    componentDidMount() {
+        // TODO - fetch creator, title, html, and css for each post from /getPosts
+    }
+
     render() {
+        const { posts } = this.state
         return (
             <div>
-                <PreviewLayout />
+                <div className='previewLayout'>
+                    { posts.map(post => <Preview html={ post.html } css={ post.css } title={ post.title }  creator={ post.creator } />) }
+                </div>
             </div>
         )
     }
